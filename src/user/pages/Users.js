73a69d6f290efa543532f544af8158a57b36afbb
Istvan Acs <istvan.acs89@gmail.com>
2019-11-30 18:51:27 +0000

    Adding a userlist page and component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>hello user</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Istvan Acs',
+      image:
+        'https://images.pexels.com/photos/34600/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=750&w=1260',
+      places: 3
+    }
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
