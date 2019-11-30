cd671f0820768039fc5768b1acc830f1f8e9bc15
Istvan Acs <istvan.acs89@gmail.com>
2019-11-30 20:19:25 +0000

    Adding navlinks

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './MainNavigation.css';
 import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
+import './MainNavigation.css';
 
 const MainNavigation = props => {
   return (
@@ -15,7 +16,9 @@ const MainNavigation = props => {
       <h1 className='main-navigation__title'>
         <Link to='/'>YourPlaces</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
