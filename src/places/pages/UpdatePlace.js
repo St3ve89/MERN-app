4762a18cd8a595074204455d3f3cae9ad0cdef5b
Istvan Acs <istvan.acs89@gmail.com>
2019-12-02 18:55:31 +0000

    Start working on the UpdatePlace page

---
+++ src/places/pages/UpdatePlace.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from '../../shared/util/validators';
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://images.pexels.com/photos/2568906/pexels-photo-2568906.png?auto=compress&cs=tinysrgb&h=750&w=1260',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484445,
+      lng: -73.9878531
+    },
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://images.pexels.com/photos/2568906/pexels-photo-2568906.png?auto=compress&cs=tinysrgb&h=750&w=1260',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484445,
+      lng: -73.9878531
+    },
+    creator: 'u2'
+  }
+];
+
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
+
+  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+
+  if (!identifiedPlace) {
+    return (
+      <div className='center'>
+        <h2>Could not find place!</h2>
+      </div>
+    );
+  }
+
+  return (
+    <form>
+      <Input
+        id='title'
+        element='input'
+        type='text'
+        label='title'
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText='Please enter a valid title.'
+        onInput={() => {}}
+        value={identifiedPlace.title}
+        valid={true}
+      />
+
+      <Input
+        id='description'
+        element='textarea'
+        label='Description'
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText='Please enter a valid description (min. 5 characters).'
+        onInput={() => {}}
+        value={identifiedPlace.description}
+        valid={true}
+      />
+      <Button type='submit' disabled={true}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
