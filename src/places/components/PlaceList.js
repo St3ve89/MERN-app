be31836f2b3c3294390d5263f36fd5e37d02f151
Istvan Acs <istvan.acs89@gmail.com>
2019-12-02 20:24:13 +0000

    Fixing minor issues

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
-import './PlaceList.css';
 import PlaceItem from './PlaceItem';
+import Button from '../../shared/components/FormElements/Button';
+import './PlaceList.css';
 
 const PlaceList = props => {
   if (props.items.length === 0) {
@@ -10,7 +11,7 @@ const PlaceList = props => {
       <div className='place-list center'>
         <Card>
           <h2> No places found. Maybe create one?</h2>
-          <button>Share Place</button>
+          <Button to='/places/new'>Share Place</Button>
         </Card>
       </div>
     );
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -19,7 +19,7 @@ const DUMMY_PLACES = [
   },
   {
     id: 'p2',
-    title: 'Empire State Building',
+    title: 'Emp. State Building',
     description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
       'https://images.pexels.com/photos/2568906/pexels-photo-2568906.png?auto=compress&cs=tinysrgb&h=750&w=1260',
