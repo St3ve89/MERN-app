dff8ef86ee90276688acedbc8a05ac791a6c19dc
Istvan Acs <istvan.acs89@gmail.com>
2019-12-07 19:01:47 +0000

    Implementing basic routing

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const placesRouters = require('./routes/places-routes');
+
+const app = express();
+
+app.use(placesRouters);
+
+app.listen(5000, () => {
+  console.log('App listening on port 5000!');
+});
