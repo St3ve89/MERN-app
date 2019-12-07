af0d1000e8755a5b7a81cf6d492defb7df29663a
Istvan Acs <istvan.acs89@gmail.com>
2019-12-07 20:17:19 +0000

    Handling Errors

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,15 @@ const app = express();
 
 app.use('/api/places/', placesRouters);
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500);
+  res.json({ success: error.message || 'An unknown error occured!' });
+});
+
 app.listen(5000, () => {
   console.log('App listening on port 5000!');
 });
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -19,12 +19,26 @@ const DUMMY_PLACES = [
 router.get('/:pid', (req, res, next) => {
   const placeId = req.params.pid;
   const place = DUMMY_PLACES.find(p => p.id === placeId);
+
+  if (!place) {
+    const error = new Error('Could not find a place for the provided id.');
+    error.code = 404;
+    throw error;
+  }
+
   res.json({ success: true, data: place });
 });
 
 router.get('/user/:uid', (req, res, next) => {
   const userId = req.params.uid;
   const place = DUMMY_PLACES.find(p => p.creator === userId);
+
+  if (!place) {
+    const error = new Error('Could not find a place for the provided user id.');
+    error.code = 404;
+    return next(error);
+  }
+
   res.json({ success: true, data: place });
 });
 
